d0a5df5ec5247432a50d4f69119dcf6cf81c4f84
Cojocariu Emanuel <129368785+cojomanu@users.noreply.github.com>
2024-12-27 14:58:04 +0200

    anulare administrare magazin

--- Magazin_online/AdministrareMagazin.cs
+++ Magazin_online/AdministrareMagazin.cs
@@ -1,20 +1,7 @@
 ﻿namespace Magazin_online;
 
-public class AdministrareMagazin: Magazin
+public class AdministrareMagazin: 
 {
     
-    public AdministrareMagazin(string nume) : base(nume) { }
-    public override void AdaugaProdus(ProdusGeneric produs)
-    {
-        Produse.Add(produs);
-    }
-
-    public override void AfiseazaProduse()
-    {
-        Console.WriteLine("Produsele sunt : ");
-        foreach (var produs in Produse)
-        {
-            Console.WriteLine(produs);
-        }
-    }
+   
 }
