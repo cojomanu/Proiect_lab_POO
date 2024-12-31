974c4e3ae86625bc6c496d9731b2377e5ac042c3
Cojocariu Emanuel <129368785+cojomanu@users.noreply.github.com>
2024-12-31 10:59:52 +0200

    gestionare erori

---
+++ Magazin_online/ErrorHandler.cs
@@ -0,0 +1,48 @@
+﻿namespace Magazin_online;
+
+public static class ErrorHandler
+{
+    public static void HandleException(Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+
+    public static int TryParseInt(string input)
+    {
+        try
+        {
+            return int.Parse(input);
+        }
+        catch (FormatException ex)
+        {
+            HandleException(ex);
+            return -1;
+        }
+    }
+
+    public static decimal TryParseDecimal(string input)
+    {
+        try
+        {
+            return decimal.Parse(input);
+        }
+        catch (FormatException ex)
+        {
+            HandleException(ex);
+            return -1m;
+        }
+    }
+
+    public static DateTime TryParseDateTime(string input)
+    {
+        try
+        {
+            return DateTime.Parse(input);
+        }
+        catch (FormatException ex)
+        {
+            HandleException(ex);
+            return DateTime.MinValue;
+        }
+    }
+}
