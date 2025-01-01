4cfe7c539ef98eefe134e8c5e53515b814bbff76
Cojocariu Emanuel <129368785+cojomanu@users.noreply.github.com>
2025-01-01 16:34:20 +0200

    mica corectie

--- Magazin_online/ErrorHandler.cs
+++ Magazin_online/ErrorHandler.cs
@@ -1,18 +1,5 @@
 ﻿using Magazin_online;
 namespace Magazin_online;
-
-
-// public static class ErrorHandler
-// {
-//     public static void HandleException(Exception ex)
-//     {
-//         Console.ForegroundColor = ConsoleColor.Red;
-//         Console.WriteLine($"Eroare: {ex.Message}");
-//         Console.ResetColor();
-//     }
-// }
-
-
 public static class ErrorHandler
 {
     public static void HandleException(Exception ex)
